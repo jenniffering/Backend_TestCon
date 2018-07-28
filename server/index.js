13f5cb4578d12938bc67e645ebc6913f2e09d74b
Jenniffer Helena Alvarez Puello <jenniffering@outlook.com>
2018-07-27 19:40:27 -0500

    Schema

--- server/index.js
+++ server/index.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
 const { mongoose } = require('./database');
 
 //settings
 app.set('port', process.env.PORT || 3000);
+
 //middlewares
+app.use(morgan('dev'));
+app.use(express.json());
 
 //routes
 
